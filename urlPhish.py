147724fc147c4d3e7a81873cd026c177766f2e41
Charles The Great <27988707+CharlesTheGreat77@users.noreply.github.com>
2022-12-23 19:38:08 -0800

    Update urlPhish.py

--- urlPhish.py
+++ urlPhish.py
@@ -1,32 +1,26 @@
 import requests
 import argparse
 
-def maskUrl(url, mask, words):
-	phishUrl = mask + '-' + words + '@' + url
-	return phishUrl
+def maskUrl(url, spoof):
 
-
-def getUrl(url):
-	response = requests.get('https://is.gd/create.php?format=simple&url=' + url)
-	shortUrl = response.text
-	shortUrl = shortUrl.replace("https://", "")
-	return shortUrl
+	phisher = f"https%c{spoof}@{url}"
+	return phisher
 
 def Main():
 	parser = argparse.ArgumentParser(description="Mask Phishing Urls")
 	parser.add_argument("-u", "--url", help="specify phishing url")
 	parser.add_argument("-m", "--mask", help="specify domain name to mask [https/http]")
-	parser.add_argument("-w", "--words", help="specify words for social engineering ['-' between words]")
 	args = parser.parse_args()
 
 	url = args.url
 	mask = args.mask
-	words = args.words
 
-	short = getUrl(url)
-	mask = maskUrl(short, mask, words)
+	url = url.replace('https://', '')
+	spoof = mask.replace('https://', '')
 
-	print("\033[1;33;40m[!] Phishing Url: " + mask)
+	phisher = maskUrl(url, spoof)
+
+	print("[!] Phishing Url: " + phisher)
 
 if __name__=="__main__":
 	Main()
